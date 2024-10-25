f8412847b1dcf3515aeb4a1b0503b36ca9d512b4
Corey Ferguson <corey.t.ferguson@gmail.com>
2024-10-25 14:30:43 -0700

    jest testing infra

---
+++ asciiPrinter.js
@@ -0,0 +1,5 @@
+
+class AsciiPrinter {
+}
+
+module.exports = AsciiPrinter;
---
+++ asciiPrinter.test.js
@@ -0,0 +1,7 @@
+const AsciiPrinter = require("./asciiPrinter");
+
+describe("Test Suite Name", () => {
+  test("Description of test case", () => {
+    console.log('here');
+  });
+});
