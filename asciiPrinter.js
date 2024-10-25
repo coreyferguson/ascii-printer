a7e33b7f8d340f3c910b11cfb6d421666cd5f24a
Corey Ferguson <corey.t.ferguson@gmail.com>
2024-10-25 14:50:27 -0700

    test case 1, L and R rectangles

--- asciiPrinter.js
+++ asciiPrinter.js
@@ -1,5 +1,36 @@
 
+const MAX_WIDTH = 10;
+const MAX_HEIGHT = 6;
+
 class AsciiPrinter {
+
+    rectangles = [];
+
+    drawRectangle(fillCharacter, leftX, topY, rightX, bottomY) {
+        this.rectangles.push({ fillCharacter, leftX, topY, rightX, bottomY });
+    }
+
+    eraseArea(leftX, topY, rightX, bottomY) {
+    }
+
+    dragAndDrop(selectX, selectY, releaseX, releaseY) {
+    }
+
+    bringToFront(selectX, selectY) {
+    }
+
+    printCanvas() {
+        const res = Array.from({ length: MAX_WIDTH }, () => Array(MAX_HEIGHT).fill(''));
+        for (let rectangle of this.rectangles) {
+            for (let y=rectangle.topY; y<=rectangle.bottomY; y++) {
+                for (let x=rectangle.leftX; x<=rectangle.rightX; x++) {
+                    res[x][y] = rectangle.fillCharacter;
+                }
+            }
+        }
+        return res;
+    }
+
 }
 
 module.exports = AsciiPrinter;
--- asciiPrinter.test.js
+++ asciiPrinter.test.js
@@ -1,7 +1,24 @@
 const AsciiPrinter = require("./asciiPrinter");
 
-describe("Test Suite Name", () => {
-  test("Description of test case", () => {
-    console.log('here');
+describe("ascii printer", () => {
+  test('an empty result has all empty strings at max size', () => {
+    const printer = new AsciiPrinter();
+    const result = printer.printCanvas();
+    expect(result[0][0]).toBe('');
+    expect(result[1][1]).toBe('');
+    expect(result[9][5]).toBe('');
+    expect(result.length).toBe(10);
+    expect(result[0].length).toBe(6);
+  });
+
+  test("test case 1, L and R rectangles", () => {
+    const printer = new AsciiPrinter();
+    printer.drawRectangle('L', 1, 1, 4, 4);
+    printer.drawRectangle('R', 6, 1, 8, 3);
+    const result = printer.printCanvas();
+    expect(result[1][1]).toBe('L');
+    expect(result[4][4]).toBe('L');
+    expect(result[6][1]).toBe('R');
+    expect(result[8][3]).toBe('R');
   });
 });
